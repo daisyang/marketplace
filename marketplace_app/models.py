790da3240a4d93003a1bc9a70000e753c2e0b976
John Anderson <sontek@gmail.com>
2014-05-03 13:49:27 -0400

    daisy version 2

--- marketplace_app/models.py
+++ marketplace_app/models.py
@@ -2,28 +2,45 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+class School (models.Model):
+    HIGH_SCHOOL = 'HI'
+    ELEMENTARY_SCHOOL = 'EL'
+    MIDDLE_SCHOOL = 'MI'
+    SCHOOL_CHOICES = (
+            (HIGH_SCHOOL, 'High shool'),
+            (ELEMENTARY_SCHOOL,'Elementary school'),
+            (MIDDLE_SCHOOL,'Middle school'),
+
+            )
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    website = models.URLField(max_length=200)
+    school_level = models.CharField(max_length=2,choices = SCHOOL_CHOICES,default=HIGH_SCHOOL)
+    funding_amount = models.FloatField()
+    review = models.IntegerField(default=0)
+
 class Provider (models.Model):
     HIGH_SCHOOL = 'HI'
     ELEMENTARY_SCHOOL = 'EL'
     MIDDLE_SCHOOL = 'MI'
     SCHOOL_CHOICES = (
-            (HIGH_SCHOOL, 'High shool').
+            (HIGH_SCHOOL, 'High shool'),
             (ELEMENTARY_SCHOOL,'Elementary school'),
             (MIDDLE_SCHOOL,'Middle school'),
 
             )
 
-	name = models.CharField(max_length=50)
-	address = models.CharField(max_length=200)
-        website = models.URLField(max_length=200)
-        contract = models.CharField(max_length=50)
-        perception = models.CharField(max_length=200)
-        target_schools = 
-        time_start= models.DateTimeField('date start')
-        time_end = models.DateTimeField('date end')
-        school_level = models.CharField(max_length=2,choices = SCHOOL_CHOICES,default=HIGH_SCHOOL)
-        funding_amount = models.FloatField()
-        non_profit = models.BooleanField()
-
-
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    website = models.URLField(max_length=200)
+    contract = models.CharField(max_length=50)
+    perception = models.CharField(max_length=200)
+    target_schools = models.ManyToManyField(School)
+    time_start= models.DateTimeField('date start')
+    time_end = models.DateTimeField('date end')
+    school_level = models.CharField(max_length=2,choices = SCHOOL_CHOICES,default=HIGH_SCHOOL)
+    funding_amount = models.FloatField()
+    non_profit = models.BooleanField()
+    contacted_before = models.BooleanField()
+    review = models.IntegerField(default=0)
 
