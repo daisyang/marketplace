c66fe0dc42633f7e9463d5cde0fbb79e8977f0e5
John Anderson <sontek@gmail.com>
2014-05-03 11:55:17 -0400

    daisy model v-1

--- marketplace_app/models.py
+++ marketplace_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Provider (models.Model):
+    HIGH_SCHOOL = 'HI'
+    ELEMENTARY_SCHOOL = 'EL'
+    MIDDLE_SCHOOL = 'MI'
+    SCHOOL_CHOICES = (
+            (HIGH_SCHOOL, 'High shool').
+            (ELEMENTARY_SCHOOL,'Elementary school'),
+            (MIDDLE_SCHOOL,'Middle school'),
+
+            )
+
+	name = models.CharField(max_length=50)
+	address = models.CharField(max_length=200)
+        website = models.URLField(max_length=200)
+        contract = models.CharField(max_length=50)
+        perception = models.CharField(max_length=200)
+        target_schools = 
+        time_start= models.DateTimeField('date start')
+        time_end = models.DateTimeField('date end')
+        school_level = models.CharField(max_length=2,choices = SCHOOL_CHOICES,default=HIGH_SCHOOL)
+        funding_amount = models.FloatField()
+        non_profit = models.BooleanField()
+
+
+
